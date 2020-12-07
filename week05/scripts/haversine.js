2cab9b0ece32bfe2d92a77f89c3ca2d5cab5cccf
luqmanrodzi <luqmanrodzi23@gmail.com>
2020-12-07 21:44:02 +0800

    0945

--- week05/scripts/haversine.js
+++ week05/scripts/haversine.js
@@ -21,6 +21,8 @@ if(typeof Number.prototype.toRad == "undefined"){
 function roundVal(val) {
     let dec = 2 ;
     let result = Math.round(val * Math.pow(10, dec)) / Math.pow(10, dec);
+
+    return result;
 }
 
 function calculateDistances(lat, lon) {
