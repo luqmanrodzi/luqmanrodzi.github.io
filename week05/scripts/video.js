38d57e80d42ac22b01bf0e36bbf4de4e9a43c1bd
luqmanrodzi <luqmanrodzi23@gmail.com>
2020-12-08 20:42:59 +0800

    0845

---
+++ week05/scripts/video.js
@@ -0,0 +1,14 @@
+window.onload = function(){
+    let video = document.getElementById("myVideo");
+
+    let playBtn = document.getElementById("playBtn");
+    playBtn.addEventListener("click", function(){
+        video.play();
+    });
+
+ let pauseBtn = document.getElementById("pauseBtn");
+    playBtn.addEventListener("click", function(){
+        video.pause();
+    });
+    
+}
