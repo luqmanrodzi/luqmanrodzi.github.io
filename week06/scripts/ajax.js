2f375ebc43059fcc2e8749da9ceb098fde1d5177
luqmanrodzi <luqmanrodzi23@gmail.com>
2020-12-11 21:30:26 +0800

    2130

--- week06/scripts/ajax.js
+++ week06/scripts/ajax.js
@@ -1,12 +1,12 @@
 function getRandomUser() {
-    let xhttp =  new XMLHttpRequest(); //create request obj
+    let xhttp = new XMLHttpRequest(); //create request obj
 
-    xhttp.onreadystatechange = function (){
-        if(this.readyState == 4 && this.status == 200) {
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
             let data = JSON.parse(this.response);
             let elFirstName = document.getElementById("firstName");
-            let ellastName = document.getElementById("lastName");
-            let eljsonResult = document.getElementById("jsonResult");
+            let elLastName = document.getElementById("lastName");
+            let elJsonResult = document.getElementById("jsonResult");
             let elUserImage = document.getElementById("userImage");
             let elPhone = document.getElementById("phone");
             let elAddress = document.getElementById("address");
@@ -15,17 +15,24 @@ function getRandomUser() {
             elFirstName.innerHTML = data.results[0].name.first;
             elUserImage.src = data.results[0].picture.large;
             elUserImage.title = data.results[0].name.first + " " + data.results[0].name.last;
+            elLastName.innerHTML = data.results[0].name.last;
+            elPhone.innerHTML = data.results[0].phone;
+            elAddress.innerHTML = data.results[0].location.street.number + ", " +
+                data.results[0].location.street.name + ", <br>"
+                + data.results[0].location.postcode + " "
+                + data.results[0].location.city + ",<br>"
+                + data.results[0].location.country;
+            elEmail.innerHTML = data.results[0].email;
 
-            eljsonResult.innerHTML = this.response;
+            elJsonResult.innerHTML = this.response;
         }
-
     }
 
     xhttp.open("GET", "https://randomuser.me/api/", true);
     xhttp.send();
 }
 
-let elGetRandomUser = document.getElementById("getRandomUserBtn")
-elGetRandomUser.addEventListener("click", function() {
+let elGetRandomUser = document.getElementById("getRandomUserBtn");
+elGetRandomUser.addEventListener("click", function () {
     getRandomUser();
 });
