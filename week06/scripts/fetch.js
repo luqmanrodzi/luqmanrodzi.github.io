1761944a6e825db28571a62a62f7f208b5753d5a
luqmanrodzi <luqmanrodzi23@gmail.com>
2020-12-15 20:46:26 +0800

    2046

--- week06/scripts/fetch.js
+++ week06/scripts/fetch.js
@@ -5,5 +5,22 @@ function GetBookings(){
         .then(json => {
             // Do something with the data
              console.log(json.bookings);
+             let bookingList = document.getElementById("bookingList");
+             bookingList.innerHTML = "";
+             for(let i = 0; i < json.bookings.length;i++){
+                 let gName = json.bookings[i].name;
+                 let gEmail = json.bookings[i].email;
+                 let gPax = json.bookings[i].pax;
+                 let gId = json.bookings[i].id;
+
+                bookingList.innerHTML += gId + "-" + gName + "," + gEmail 
+                + ", pax:" + pax + "<br>";
+            
+             }
         });
 }
+
+let getBookingBtn = document.getElementById("getbooking");
+getBookingBtn.addEventListener("click", function (){
+    GetBookings();
+});
