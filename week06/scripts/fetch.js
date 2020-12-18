695c5eb08a422775cd5fed5aef2121ecff83e5ba
luqmanrodzi <luqmanrodzi23@gmail.com>
2020-12-18 19:55:18 +0800

    1950

--- week06/scripts/fetch.js
+++ week06/scripts/fetch.js
@@ -71,8 +71,7 @@ let url = 'https://api.sheety.co/fd818c0c3e27fb12c0cc5d26545b8da8/bookingApp/boo
     fetch(url, {
       method: 'DELETE',
   })
-    .then((response) => response.json())
-    .then(() => {
+    .then((response) => {
       document.getElementById("deleteMsg").innerHTML = "Booking Delete!";
       getBooking();
     });
