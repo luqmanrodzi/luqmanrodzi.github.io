e11bc9c42d41759112d7bb9d08e23c9c420c19e3
noboarder <noboarder.magazine@gmail.com>
2020-12-22 21:36:50 +0800

    2135

--- tableapp/scripts/list.js
+++ tableapp/scripts/list.js
@@ -1,12 +1,16 @@
 function GetBookings() {
     let url = 'https://api.sheety.co/fd818c0c3e27fb12c0cc5d26545b8da8/bookingApp/bookings';
-    fetch(url)
         .then((response) => response.json())
         .then(json => {
             // Do something with the data
             //console.log(json.bookings);
             let bookingList = document.getElementById("booking-list");
             let bookingIds = [];
+
+            //clear the table rows
+            for (let k = bookingList.rows.length - 1; k > 0; k--) {
+                bookingList.deleteRow(k);
+            }
 
             for (let i = 0; i < json.bookings.length; i++) {
                 let gName = json.bookings[i].name;
@@ -50,6 +54,6 @@ function DeleteBooking(id) {
         method: 'DELETE',
     })
         .then((response) => {
-            location.reload();
+            GetBookings();
         });
 }
