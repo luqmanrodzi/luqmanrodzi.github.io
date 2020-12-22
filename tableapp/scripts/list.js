db99846bdb0b675c816f8af01edd4bf49557d26b
noboarder <noboarder.magazine@gmail.com>
2020-12-22 21:32:12 +0800

    2130

--- tableapp/scripts/list.js
+++ tableapp/scripts/list.js
@@ -8,6 +8,11 @@ function GetBookings() {
             let bookingList = document.getElementById("booking-list");
             let bookingIds = [];
 
+            //clear the table rows
+            for(let k = bookingList.row.length - 1; k > 0; k--){
+                bookingList.deleteRow(k);
+            }
+
             for (let i = 0; i < json.bookings.length; i++) {
                 let gName = json.bookings[i].name;
                 let gEmail = json.bookings[i].email;
@@ -44,11 +49,12 @@ window.addEventListener("load", function () {
     });
 })
 
-function DeleteBooking(id){
+function DeleteBooking(id) {
     let url = 'https://api.sheety.co/fd818c0c3e27fb12c0cc5d26545b8da8/bookingApp/bookings/2' + id;
-        fetch(url, {
+    fetch(url, {
         method: 'DELETE',
     })
         .then((response) => {
-            location.reload();
-    });
+            GetBookings();
+        });
+}
