79dc433b51bb85da08660b9607c4c34d81ea549b
noboarder <noboarder.magazine@gmail.com>
2020-12-22 20:55:07 +0800

    2056

--- tableapp/scripts/book.js
+++ tableapp/scripts/book.js
@@ -1,25 +1,24 @@
-function BookNow(guestName,guestEmail,guestPax, guestRemarks){
+function BookNow(guestName, guestEmail, guestPax, guestRemarks) {
     let url = 'https://api.sheety.co/fd818c0c3e27fb12c0cc5d26545b8da8/bookingApp/bookings';
     let body = {
         booking: {
-            name:guestName,
-            email:guestEmail,
-            pax:userPax,
-            remarks: userRemarks
+            name: guestName,
+            email: guestEmail,
+            pax: guestPax,
+            remarks: guestRemarks
         }
     }
     fetch(url, {
         method: 'POST',
         body: JSON.stringify(body),
-        headers:{
-            "Content-Type":"application/json"
+        headers: {
+            "Content-Type": "application/json"
         }
     })
         .then((response) => response.json())
         .then(json => {
-            alert(jason.booking.name + "added!`);
-        
-    });
+            alert(json.booking.name + " added!");
+        });
   }
 
   window.addEventListener("load", function() {
